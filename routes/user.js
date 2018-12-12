9f0306c3a0b701794f3dd6f799b2b2fc4ed1af24
pharaj <pharaj@excellencetechnologies.in>
2018-12-12 14:30:02 +0530

    all files in es6 format

--- routes/user.js
+++ routes/user.js
@@ -4,16 +4,15 @@ const User = require('../collections/usermodel');
 const UserProfile = require('../collections/userprofilemodel');
 const { check, validationResult } = require('express-validator/check')
 
-function validateToken(req, res, next) {
-    var token = req.headers['authorization']
+const validateToken = async (req, res, next) => {
+    let token = req.headers['authorization']
     if (token) {
-        User.findById(token).then(result => {
-            if (result) {
-                next();
-            } else {
-                res.status(500).send('forbidden');
-            }
-        })
+        const result = await User.findById(token);
+        if (result) {
+            next();
+        } else {
+            res.status(500).send('forbidden')
+        }
     } else {
         res.status(500).send('forbidden**');
     }
@@ -27,34 +26,31 @@ router.post('/register', [
     check('user_name', "user name is required").not().isEmpty(),
     check('password', "password is required").not().isEmpty(),
     check('confirm_password', "confirm_password is required").not().isEmpty()
-], (req, res, next) => {
+], async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         res.status(400).send(errors.array());
     } else {
         if (req.body.password == req.body.confirm_password) {
-            User.findOne({ email: req.body.email }).then((result) => {
-                if (result) {
-                    res.status(400).send('email already saved');
+            const result = await User.findOne({ email: req.body.email });
+            if (result) {
+                res.status(400).send('email already saved');
+            } else {
+                const response = await User.findOne({ user_name: req.body.user_name });
+                if (response) {
+                    res.status(400).send('username already existed');
                 } else {
-                    User.findOne({ user_name: req.body.user_name }).then((response) => {
-                        if (response) {
-                            res.status(400).send('username already existed');
-                        } else {
-                            let newUser = new User();
-                            newUser.first_name = req.body.first_name;
-                            newUser.last_name = req.body.last_name;
-                            newUser.user_name = req.body.user_name;
-                            newUser.email = req.body.email;
-                            newUser.setPassword(req.body.password);
-                            newUser.save().then(result => {
-                                res.send(result);
-                            })
-                        }
-                    })
+                    let newUser = new User();
+                    newUser.first_name = req.body.first_name;
+                    newUser.last_name = req.body.last_name;
+                    newUser.user_name = req.body.user_name;
+                    newUser.email = req.body.email;
+                    newUser.setPassword(req.body.password);
+                    const saveUser = await newUser.save()
+                    res.json(saveUser);
                 }
-            })
+            }
         } else {
             res.send('password not matched');
         }
@@ -67,42 +63,34 @@ router.post('/register', [
 router.post('/login', [
     check('user_name', "user name is required").not().isEmpty(),
     check('password', "password is required").not().isEmpty()
-], (req, res, next) => {
+], async (req, res, next) => {
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
         res.status(400).send(errors.array());
     } else {
-        User.findOne({ user_name: req.body.user_name }).then(result => {
-            if (result) {
-                if (result.validPassword(req.body.password)) {
-                    res.send(result._id);
-                } else {
-                    res.status(500).send('password is incorrect');
-                }
-
+        const result = await User.findOne({ user_name: req.body.user_name })
+        if (result) {
+            if (result.validPassword(req.body.password)) {
+                res.json(result._id);
             } else {
-                res.status(500).send('User name incorrect');
+                res.status(500).send('password is incorrect');
             }
-        })
+        } else {
+            res.status(500).send('User name incorrect');
+        }
     }
 });
 
-
 //route to get user data
-router.get('/get', validateToken, (req, res, next) => {
-    User.find({}).then(data => {
-        res.json(data);
-    })
+router.get('/get', validateToken, async (req, res, next) => {
+    const response = await User.find({});
+    res.json(response);
 });
 
-
 //route to delete user data
-router.put('/delete', validateToken, (req, res, next) => {
-    User.findOneAndDelete({ _id: req.headers['authorization'] }).then(data => {
-        //deleted user data
-        res.json(data);
-    })
+router.put('/delete', validateToken, async (req, res, next) => {
+    const response = await User.findOneAndDelete({ _id: req.headers['authorization'] });
+    res.json(response);
 })
 
 
